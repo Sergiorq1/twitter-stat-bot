4ee259b69fdc53e2e5db927c7381e6f83def8a22
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2022-04-19 18:44:32 -0600

    isted indices to transfer in respective tables

--- bots/seasonPPG.py
+++ bots/seasonPPG.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import sqlite3
 import contextlib
-
 # database connection
 conn = sqlite3.connect('my_database.db')
 cur = conn.cursor()
@@ -13,7 +12,6 @@ cur = conn.cursor()
 #url I'm scraping
 url = "https://www.basketball-reference.com/leagues/NBA_2022_per_game.html#per_game_stats::pts_per_g"
 
-# url = "https://www.basketball-reference.com/playoffs/"
 # collect html data
 html = urlopen(url)
 #create beautiful soup object from HTML
@@ -23,14 +21,18 @@ soup = BeautifulSoup(html, 'html.parser')
 headers = [th.getText() for th in soup.findAll('tr') 
 [0].findAll('th')]
 headers = headers[1:]
-headers = ["field1", "field2", "field3"]
-print(headers)
-
-
+Gheaders = headers[:7]
+BOFFindices = [7,8,10,11,13,14,17,18,23,28]
+AOFFindices = [9,12,15,16,19]
+DEFindices = [24,25]
+OTHERindices = [20,21,22,26,27]
+BOFFheaders = [headers[index] for index in BOFFindices]
+print(BOFFheaders)
 #  GENERAL Table
 cur.execute('DROP TABLE IF EXISTS GENERAL')
 cur.execute('''CREATE TABLE GENERAL 
             (Player text, Pos text, Age INTEGER, Tm text, G INTEGER, GS INTEGER, MP real)''')
+cur.execute('INSERT INTO GENERAL VALUES(?,?,?,?,?,?,?)', Gheaders)
 conn.commit()
 cur.execute('SELECT * FROM GENERAL')
 print(f'this is general table: {cur.fetchall()}')
@@ -39,33 +41,35 @@ print(f'this is general table: {cur.fetchall()}')
 cur.execute('DROP TABLE IF EXISTS BOFF')
 cur.execute('''CREATE TABLE BOFF 
             (FG real, FGA real, 3P real, 3PA real, 2P real, 2PA real, FT real, FTA real, AST real, PTS real)''')
+cur.execute('INSERT INTO GENERAL VALUES(?,?,?,?,?,?,?,?,?,?)', BOFFheaders)
 conn.commit()
 cur.execute('SELECT * FROM BOFF')
 print(f'this is BOFF table: {cur.fetchall()}')
 
-# AOFF Table
-cur.execute('DROP TABLE IF EXISTS AOFF')
-cur.execute('''CREATE TABLE AOFF 
-            (FGP real, 3PP real, 2PP real, eFGP real, FTP real)''')
-conn.commit()
-cur.execute('SELECT * FROM AOFF')
-print(f'this is AOFF table: {cur.fetchall()}')
+# # AOFF Table 
+# cur.execute('DROP TABLE IF EXISTS AOFF')
+# cur.execute('''CREATE TABLE AOFF 
+#             (FGP real, 3PP real, 2PP real, eFGP real, FTP real)''')
+# conn.commit()
+# cur.execute('SELECT * FROM AOFF')
+# print(f'this is AOFF table: {cur.fetchall()}')
+Player;Pos;Age;Tm;G;GS;MP;FG;FGA;FG%; 103P; 113PA; 123P%;13 2P;14 2PA;15 2P%;16 eFG%;17 FT;18 FTA;FT%;ORB;DRB;TRB;23AST;STL;BLK;TOV;PF;28PTS
 
-# DEF Table
-cur.execute('DROP TABLE IF EXISTS DEF')
-cur.execute('''CREATE TABLE DEF 
-            (STL real, BLK real, TOV real, PF real)''')
-conn.commit()
-cur.execute('SELECT * FROM DEF')
-print(f'this is DEF table: {cur.fetchall()}')
+# # DEF Table 
+# cur.execute('DROP TABLE IF EXISTS DEF')
+# cur.execute('''CREATE TABLE DEF 
+#             (STL real, BLK real)''')
+# conn.commit()
+# cur.execute('SELECT * FROM DEF')
+# print(f'this is DEF table: {cur.fetchall()}')
 
-# OTHER Table
-cur.execute('DROP TABLE IF EXISTS OTHER')
-cur.execute('''CREATE TABLE AOFF 
-            (ORB real, DRB real, TRB real, PF real)''')
-conn.commit()
-cur.execute('SELECT * FROM OTHER')
-print(f'this is OTHER table: {cur.fetchall()}')
+# # OTHER Table 
+# cur.execute('DROP TABLE IF EXISTS OTHER')
+# cur.execute('''CREATE TABLE AOFF 
+#             (ORB real, DRB real, TRB real, TOV real, PF real)''')
+# conn.commit()
+# cur.execute('SELECT * FROM OTHER')
+# print(f'this is OTHER table: {cur.fetchall()}')
 
 
 #get rows from table
